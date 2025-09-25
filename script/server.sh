186a1ceeac0fc11358d313a047d797e62ff4063f
12sex12 <pesonjdr@gmail.com>
2025-09-25 18:17:42 +0300

    Update server.sh
    10gb of memory cu yes

--- script/server.sh
+++ script/server.sh
@@ -43,7 +43,7 @@ while true; do
 	# Start Minecraft server
 
 	java \
-		-Xms3400M -Xmx3400M \
+		-Xms10400M -Xmx10400M \
 		\
 		-Dusing.aikars.flags=https://mcflags.emc.gs -Daikars.new.flags=true \
 		-XX:+UseG1GC \
